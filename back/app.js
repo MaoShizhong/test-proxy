a8b02172e809fab2c6a95a1e7fc150fa4ce64bfc
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-05-27 18:04:02 +0100

    no trust proxy

---
+++ back/app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const app = require("express")();
+const session = require("express-session");
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: ":eyes:",
+    cookie: {
+      name: "foo",
+      sameSite: "none",
+      httpOnly: true,
+      secure: true,
+    },
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.json(req.session.userId || "no user");
+});
+app.post("/sessions", (req, res, next) => {
+  req.session.userId = "logged in";
+  req.session.save((err) => {
+    if (err) {
+      next(err);
+    } else {
+      res.json("logged in");
+    }
+  });
+});
+app.delete("/sessions", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+    } else {
+      res.json("logged out");
+    }
+  });
+});
+
+app.listen(3000, () => console.log("Server listening at port 3000"));
